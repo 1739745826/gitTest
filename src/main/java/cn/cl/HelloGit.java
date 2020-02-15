460bac7b1397c0a864f05aeb2d3ecb872e70e487
tom_glb <goonMorning_glb@qq.com>
2020-02-15 14:18:17 +0800

    GIT Test 测试

---
+++ src/main/java/cn/cl/HelloGit.java
@@ -0,0 +1,12 @@
+package cn.cl;
+
+/**
+ * 标题：Hello Git
+ * 作者：何处是归程
+ * 时间：2020/2/15 - 14:11
+ */
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+}
